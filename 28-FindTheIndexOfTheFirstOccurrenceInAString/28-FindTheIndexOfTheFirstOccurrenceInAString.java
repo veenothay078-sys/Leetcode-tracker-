8b03e139b682c89865db50952ecddcbdd2df1bbc
veenothay078-sys <veenothay078@gmail.com>
2026-07-09 12:28:46 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 12:28:45 PM
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int index=haystack.indexOf(needle);
+        return index;
+        
+    }
+}
