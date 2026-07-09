7ea9b58926d1a4f6ee3800b38d66379d6c8cb9b7
veenothay078-sys <veenothay078@gmail.com>
2026-07-09 12:28:33 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 12:28:32 PM
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;  
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 5)); 
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 2)); 
+        System.out.println(solution.searchInsert(new int[]{1, 3, 5, 6}, 7)); 
+    }
+}
+    
