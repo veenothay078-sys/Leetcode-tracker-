d5bd690a483643618f97bfc2d20c69503159919e
veenothay078-sys <veenothay078@gmail.com>
2026-07-09 12:10:42 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 12:10:41 PM
+// 79% Beats
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1); // The first and last elements in each row are 1.
+                } else {
+                    int prevRow = i - 1;
+                    int leftVal = triangle.get(prevRow).get(j - 1);
+                    int rightVal = triangle.get(prevRow).get(j);
+                    row.add(leftVal + rightVal); // Sum of the two numbers above.
+                }
+            }
+            triangle.add(row);
+        }
+
+        return triangle.get(rowIndex);
+    }
+}
