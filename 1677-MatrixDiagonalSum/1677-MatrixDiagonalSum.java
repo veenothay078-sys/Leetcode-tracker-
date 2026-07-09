c02a92eb5a6104f5c0abe5ecc104eab50a51806e
veenothay078-sys <veenothay078@gmail.com>
2026-07-09 11:41:41 +0530

    Create 1677-MatrixDiagonalSum

---
+++ 1677-MatrixDiagonalSum/1677-MatrixDiagonalSum.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 11:41:40 AM
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        for(int i=0;i<mat.length;i++){
+            sum += mat[i][i];
+            if( i != mat.length-1-i){
+                sum += mat[i][mat.length-i-1];
+            }
+           
+        }
+         return sum;
+}
+}
