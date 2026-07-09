29d60ac8f36e6beeffb660c5d9944f853e3baca0
veenothay078-sys <veenothay078@gmail.com>
2026-07-09 11:41:32 +0530

    Create 2737-RowWithMaximumOnes

---
+++ 2737-RowWithMaximumOnes/2737-RowWithMaximumOnes.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 11:41:31 AM
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int maxCount=0;
+        int maxIndex=0;
+        for(int i=0;i<mat.length;i++)
+        {
+            int count=0;
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    count++;
+                }
+            }
+            if(count>maxCount)
+            {
+                maxCount = count;
+                maxIndex = i;
+            }
+  
+
+        }
+        return new int[]{maxIndex,maxCount};
+        
+    }
+}
