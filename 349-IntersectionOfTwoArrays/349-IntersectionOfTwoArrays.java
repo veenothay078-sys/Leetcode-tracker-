9f4e8584e7d65593af78f16dc80b30fa11f41753
veenothay078-sys <veenothay078@gmail.com>
2026-08-27 13:54:26 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.java
@@ -0,0 +1,19 @@
+// Last updated: 8/27/2026, 1:54:20 PM
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> s1 = new HashSet<>();
+        HashSet<Integer> ans = new HashSet<>();
+
+        for (int x : nums1) s1.add(x);
+        for (int x : nums2)
+            if (s1.contains(x))
+                ans.add(x);
+
+        int[] res = new int[ans.size()];
+        int i = 0;
+        for (int x : ans)
+            res[i++] = x;
+
+        return res;
+    }
+}
