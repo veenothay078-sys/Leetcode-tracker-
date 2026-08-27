d19efff1f496ff8bcccd6fdc6f206b3234a73f19
veenothay078-sys <veenothay078@gmail.com>
2026-08-27 13:54:45 +0530

    Create 318-MaximumProductOfWordLengths

---
+++ 318-MaximumProductOfWordLengths/318-MaximumProductOfWordLengths.java
@@ -0,0 +1,19 @@
+// Last updated: 8/27/2026, 1:54:38 PM
+class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int[] masks = new int[n];
+        
+        for (int i=0; i<n; i++)
+            for (char c: words[i].toCharArray())
+                masks[i] |= (1 << (c - 'a'));
+        
+        int largest = 0;
+        for (int i=0; i<n-1; i++) 
+            for (int j=i+1; j<n; j++) 
+                if ((masks[i] & masks[j]) == 0) 
+					largest = Math.max(largest, words[i].length() * words[j].length());
+        
+        return largest;
+    }
+}
